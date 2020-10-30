a6faed6bfa9ba33d9c0e9496c1f954f331938773
soufianeAe55 <boubellasoufiane@gmail.com>
2020-10-30 21:55:56 +0100

    first setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
+import LoginPage from './Components/Login/LoginPage'
 
 function App() {
   return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2> test </h2>
-        <p>
-         this dev-soufiane.
-        </p>
-
-        <li> </li>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path='/' component={LoginPage} />
+      </Switch>
     </div>
   );
 }
