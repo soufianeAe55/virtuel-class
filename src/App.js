7cfb638eb06ec2ea8faa8b1816a26a8b8b26a3fd
soufianeAe55 <boubellasoufiane@gmail.com>
2020-10-30 22:38:42 +0100

    first setup

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {Switch, Route} from 'react-router-dom';
 import LoginPage from './Components/Login/LoginPage'
 
 function App() {
+
   return ( 
     <div className="App">
       <Switch>
         <Route exact path='/' component={LoginPage} />
       </Switch>
+
     </div>
   );
 }
