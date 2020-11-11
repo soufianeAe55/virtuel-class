ee5138d79db32e27cda92c7b0bf4d288952dc087
MouadAe <mouadaouane49@gmail.com>
2020-11-11 01:54:56 +0100

    support v-3

---
+++ src/Components/Etudiant/ModalEditSupport.js
@@ -0,0 +1,62 @@
+import React,{useEffect, useState} from 'react'
+import '../../styles/SupportEtu.css'
+import File from '../Dashboard/imgs/File.svg'
+
+function ModalEditSupport() {
+   const [fileName, setfileName] = useState("Choisir un fichier")
+   const [file, setfile] = useState(null)
+
+   useEffect(() => {
+      // const actualBtn = document.getElementById('upload btn');
+
+      // const fileChosen = document.getElementById('file-chosen');
+
+      // const close = document.querySelector('#close');
+      // console.log(close)
+      // actualBtn.addEventListener('change', function(){
+      // fileChosen.textContent = this.files[0].name
+      // })
+   }, [])
+   const handleChange = e => {
+      setfileName(e.target.files[0].name);
+      setfile(e.target.files[0]) 
+   }
+
+   return (
+   <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal-dialog" role="document">
+         <form className="modal-content">
+            <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">Modifier un support</h5>
+            <button onClick={() => { setfileName("Choisir un fichier"); setfile(null) }} type="button" className="close" id="close" data-dismiss="modal" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+            </button>
+            {console.log(fileName)}
+            </div>
+            <div className="modal-body border border-dark ModalEditSupport">
+               <div className="d-flex flex-row py-2 justify-content-around border border-dark">
+                  <input type="text" className="col-5 py-3 " placeholder="Module" />
+                  <input type="text" className="col-5 py-3 " placeholder="Semestre"/>
+               </div>
+               <div className="d-flex flex-row p-2 border border-dark">
+                  <input type="text" className="w-100 mx-2 px-3 py-3 " placeholder="Nouveau Titre du support" />
+               </div>
+               <div className="d-flex flex-row p-2 border border-dark">
+                  <textarea type="text" className="w-100 mx-2 px-3 py-3 " placeholder="Ajouter une nouvelle discription" />
+               </div>
+            </div>
+            <div className="modal-footer d-flex flex-row justify-content-around ModalEditSupport__footer">
+               <input onChange={handleChange}  type="file" id="upload btn" hidden/>
+               <label className="d-flex" for="upload btn">
+                  <img src={File} alt="" />
+                  <p id="file-chosen"> { fileName }  </p>
+               </label>
+               <button type="submit">Enregistrer</button>
+            </div>
+         </form>
+      </div>
+   </div>
+)
+}
+
+export default ModalEditSupport
--- src/Components/Etudiant/SupportRow.js
+++ src/Components/Etudiant/SupportRow.js
@@ -2,11 +2,12 @@ import React from 'react'
 import IconSupportPc from '../Dashboard/imgs/IconSupportPc.svg';
 import Pen from '../Dashboard/imgs/Pen.svg';
 import Delete from '../Dashboard/imgs/delete.svg';
+import ModalEditSupport from './ModalEditSupport';
 
 
 function SupportRow() {
    return (
-      <div className="d-flex flex-column flex-sm-row SupportRow">
+      <div className="d-flex SupportRow">
          <div className="col-9 d-flex justify-content-around mr-5">
             <img className="SupportRow__img my-auto" src={IconSupportPc} alt="" />
             <div className="border border-dark d-flex flex-xl-row flex-column px-5 mx-5">
@@ -15,9 +16,11 @@ function SupportRow() {
             </div>
          </div>
          <div className="col-3 d-flex SupportRow__EditImages mx-auto my-auto">
-            <img className="border border-dark" src={Pen} alt="" />
+            <img className="border border-dark" data-toggle="modal" data-target="#exampleModal" src={Pen} alt="" />
             <img className="border border-dark" src={Delete} alt="" />
          </div>
+         <ModalEditSupport />
+
       </div>
    )
 }
