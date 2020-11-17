15493cc9de9412eebc3bc99b95315c3271fb3909
MouadAe <mouadaouane49@gmail.com>
2020-11-17 15:10:21 +0100

    prof v-1

--- src/Components/Etudiant/Myclass/ModuleCard.js
+++ src/Components/Etudiant/Myclass/ModuleCard.js
@@ -5,19 +5,8 @@ import '../../../styles/Myclass.css'
 function ModuleCard(){
 
 	return(
-<<<<<<< HEAD
 		<div className="col-12 col-lg-8 col-md-10 col-xl-8 col-sm-12 module">
 			<div className="blueLine">
-=======
-		 
-				
-			<div className="col-11 col-lg-8 col-md-10 col-xl-8 col-sm-10 module">
-				<div className="blueLine">
-				</div>
-				<Link to="/ModuleOptions" className="moduleName">
-					Module 1: Xml et les Frameworks
-				</Link>
->>>>>>> 537c6e41445459fa6dd97d33da70cc4bb150f515
 			</div>
 			<Link to="/ModuleOptions" className="moduleName">
 				Module 1: Xml et les Frameworks
--- src/Components/Etudiant/Myclass/Myclass-modules.js
+++ src/Components/Etudiant/Myclass/Myclass-modules.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import SideNav from '../../Dashboard/sideNav'
-// import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import Menu from '../../Dashboard/Menu'
 import '../../../styles/Myclass.css'
 import ModuleCard from './ModuleCard'
@@ -19,10 +19,8 @@ function MyclassModule(){
 					
 					<div className="row justify-content-center cont ">
 						<div className="row justify-content-end col-lg-12 col-xl-12 moduleHeader" >
-<<<<<<< HEAD
 							<input placeholder="Modules" className=" col-lg-3 col-xl-3 col-sm-8 col-md-6 col-10 ModuleSearch" />
 								<img className=" col-lg-2 col-xl-2 col-sm-4 col-md-4 col-2 search" src={Search} alt="" />
-=======
 							<nav className=" col-lg-12 col-xl-12 col-sm-12 col-md-12 col-12 " aria-label="breadcrumb">
 									  <ol class="breadcrumb">
 									    <li class="breadcrumb-item " ><Link to="MyClass">Semsters </Link></li>
@@ -30,7 +28,6 @@ function MyclassModule(){
 									  </ol>
 							</nav>
 	                        
->>>>>>> 537c6e41445459fa6dd97d33da70cc4bb150f515
 						</div>
 						<ModuleCard />
 						<ModuleCard />
